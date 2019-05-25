fcf8b21f844f0d2b6892d71d837bd12bba5f0d73
PabloOb <obrepalski@pm.me>
2019-05-25 21:51:23 +0200

    first class approach :

--- bot.py
+++ bot.py
@@ -1,25 +1,89 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler
 import logging
+import tokens
+import sys
+# import dataset
 
+#Python Reddit API Wrapper
+import praw
+
+# Logger setup
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+                    level=logging.INFO,
+                    stream=sys.stdout)
+
+logger = logging.getLogger(__name__)
+
+# Create reddit wrapper object
+reddit = praw.Reddit(client_id = tokens.client_id,
+                    client_secret = tokens.client_secret,
+                    username = tokens.username,
+                    password = tokens.password,
+                    user_agent = tokens.user_agent)
+
+class RedditBot():
+    """
+    Creates and mantains connection with Telegram.
+    Provides content from Reddit through Telegram chat.
+
+    Handles multiple commands (e.g. \start).
+    Subreddits can be chosen using commands.
+    """
+
+    def __init__(self):
+        # Create useful objects
+        updater = Updater(token=tokens.telegram_token, use_context=True)
+        dispatcher = updater.dispatcher
+
+        # Add Handlers
+        dispatcher.add_handler(CommandHandler('start', self.welcome))
+        dispatcher.add_handler(CommandHandler('stats', self.stats))
+        # dispatcher.add_handler(MessageHandler(self.fetch))
+        # dispatcher.add_error_handler(ErrorHandler(self.error_callback))
 
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+                # Initial values of parameters
+        self.message = ''
+        self.chat_id = None
+        self.user_id = None
+        self.subreddit = None
+        self.submission = None
 
-def hello(bot, update):
-    update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
+        # Start the bot
+        updater.start_polling()
+        logger.info("RedditBot started polling.")
 
-start_handler = CommandHandler('start', start)
-hello_handler = CommandHandler('hello', hello)
+    def welcome(self, update, context):
+        context.bot.send_message(chat_id=update.message.chat_id, text="Hello. I'm a bot, please talk to me!")
 
-updater = Updater(token='701812755:AAF8egty5DwPkrmA4LOBPz4zoHdPtdoU3XU', use_context=True)
-dispatcher = updater.dispatcher
+    def stats(self):
+        pass
 
-dispatcher.add_handler(hello_handler)
-dispatcher.add_handler(start_handler)
+    def fetch(self):
+        pass
+    
+    # def error(self):
+    #     pass
 
-updater.start_polling()
+    def error_callback(update, context):
+        logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+    # def hello(bot, update):
+    # 	update.message.reply_text(
+    # 		'Hello {}'.format(update.message.from_user.first_name))
+
+    # start_handler = CommandHandler('start', start)
+    # hello_handler = CommandHandler('hello', hello)
+
+    # updater = Updater(token=tokens.telegram_token, use_context=True)
+    # dispatcher = updater.dispatcher
+
+    # dispatcher.add_handler(hello_handler)
+    # dispatcher.add_handler(start_handler)
+
+
 # updater.idle()
+
+
+if __name__ == "__main__":
+        RedditBot()
--- praw.py
+++
@@ -1,5 +0,0 @@
-import praw 
-reddit = praw.Reddit(client_id='ID', client_secret="SECRET", user_agent='USERAGENT')       
-
-for submission in reddit.subreddit('learnpython').hot(limit=10): 
-    print(submission.title) 
